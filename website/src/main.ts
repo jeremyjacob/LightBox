647a5ad579a7f1c68c317d2018b77c5d30f56269
Jeremy Jacob <github@jeremyjacob.dev>
2022-06-15 19:24:04 -0700

    Redirect to IP

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,8 +1,12 @@
-import App from './App.svelte'
-import './global.scss'
+if (location.host == 'satislux.com') {
+	location.href = 'http://192.168.86.120';
+}
+
+import App from './App.svelte';
+import './global.scss';
 
 const app = new App({
 	target: document.getElementById('app'),
-})
+});
 
-export default app
+export default app;
